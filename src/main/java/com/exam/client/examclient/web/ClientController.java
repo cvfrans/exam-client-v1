271f1141ea5c811f44f974e837ad02e16959b231
Frans Valdivia <cvfrans@Franss-MacBook-Pro.local>
2020-01-14 00:49:48 -0500

    initial project

---
+++ src/main/java/com/exam/client/examclient/web/ClientController.java
@@ -0,0 +1,15 @@
+package com.exam.client.examclient.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/rest")
+public class ClientController {
+
+	@GetMapping(path="/listclientes", produces = "application/json")
+	public String listClientes() {
+		return "Any String";
+	}
+}
